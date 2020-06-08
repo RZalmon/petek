d52a40189f5e48f38aaecbb70887c8f1498a1d5c
Your NameTamir <tamirn7@gmail.com>
2020-06-08 11:32:14 +0300

    added sound

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { createBrowserHistory } from 'history';
 import { Router } from 'react-router-dom';
 import { connect } from 'react-redux';
 
+import audioNotification from '../src/assets/sound/notification.mp3'
+
 import SocketService from './services/SocketService'
 import { updateUser } from '../src/actions/UserActions';
 
@@ -26,11 +28,14 @@ const history = createBrowserHistory();
   }
 
   const updateUser = (updatedUser) => {
+    let audio = new Audio(audioNotification);
+
     console.log('its back!', updatedUser);
     console.log('its props!', props);
     
    if (updatedUser) {
      props.updateUser(updatedUser)
+     audio.play()
      // this.audioNotification.play();
      console.log('updated user', props);
      
