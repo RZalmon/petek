0f91ef9a4963b5f32a8ef729b20a31cf1b31727b
RZalmon <ranzlmn@gmail.com>
2020-06-28 13:20:19 +0300

    noteService no longer exist

--- src/store/index.js
+++ src/store/index.js
@@ -3,14 +3,12 @@ import thunk from 'redux-thunk';
 
 import UserReducer from './reducers/UserReducer';
 import ContactReducer from './reducers/ContactReducer';
-import NoteReducer from './reducers/NoteReducer'
 import RoomReducer from './reducers/RoomReducer'
 const composeEnhancers = compose;
 
 const rootReducer = combineReducers({
     user: UserReducer,
     contact: ContactReducer,
-    note: NoteReducer,
     room: RoomReducer
 })
 
