5bf72908039671b76e200db6049ae5a519c06a9d
RZalmon <ranzlmn@gmail.com>
2020-06-30 19:03:04 +0300

    testing img stretch on avatar edit

--- src/cmps/User/AvatarEdit.jsx
+++ src/cmps/User/AvatarEdit.jsx
@@ -6,13 +6,16 @@ import Loading from "../Loading";
 export const AvatarEdit = (props) => {
 
   // var [{imgUrl}] = useState(props)
-  const {imgUrl, isLoading} = props  
-return <div className="avatar-edit">
-          <label>
-            <input type="file" onChange={(ev) => props.onUploadImg(ev)} hidden />
-            {isLoading ? <Loading/> : <img src={imgUrl? imgUrl : avatarImg} alt="" className="avatar avatar-m"/>}
-          </label>
-    </div>;
+  const { imgUrl, isLoading } = props
+  return <div className="avatar-edit">
+    <label>
+      <input type="file" onChange={(ev) => props.onUploadImg(ev)} hidden />
+      {isLoading ? <Loading /> : <img src={imgUrl ? imgUrl : avatarImg} alt="" className="avatar avatar-m" />}
+    </label>
+    <h1>above</h1>
+    <div className="test-img" style={{ backgroundImage: `url(${imgUrl})` }}></div>
+    <h1>under</h1>
+  </div>;
 }
 
 
