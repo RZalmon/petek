173c7f0b432a459cb8154230ad59011e9b7755d6
RZalmon <ranzlmn@gmail.com>
2020-08-10 19:00:56 +0300

    nothing special

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from "react";
+
+
+import React, { Component } from "react";
 import { MDBInput } from "mdbreact";
 import Loading from "../cmps/Loading";
 
@@ -13,53 +15,82 @@ import CloudinaryService from '../../src/services/CloudinaryService'
 import { AvatarEdit } from '../cmps/User/AvatarEdit'
 
 
-const SignupPage = (props) => {
-  const [isSignup, setIsSignup] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
-  const [isDone, setIsDone] = useState(false)
-  const [newUser, setNewUser] = useState({
-    userName: '',
-    fullName: '',
-    password: '',
-    imgUrl: '',
-  })
-  let loggedinUser = null
+class SignupPage extends Component {
 
-  const getLoggedinUser = async () => {
-    await props.getUser()
-    if (props.user) props.history.push('/')
-    return props.user
-  }
-
-
-
-  const resetInput = () => {
-    setNewUser({
+  state = {
+    newUser: {
       userName: '',
       fullName: '',
       password: '',
       imgUrl: '',
+    },
+    isSignUp: false,
+    isLoading: false,
+    isDone: false
+  }
+
+  componentDidMount() {
+    console.log('hi from signUp', this.state.newUser);
+
+    this.getLoggedinUser();
+  }
+
+  getLoggedinUser = async () => {
+    await this.props.getUser()
+    if (this.props.user) this.props.history.push('/')
+  }
+
+  onChangeHandler = (ev) => {
+    const { value, name } = ev.target;
+    this.setState({
+      newUser: {
+        ...this.state.newUser,
+        [name]: value
+      }
+    })
+  };
+
+  resetInput = () => {
+    this.setState({
+      newUser: {
+        userName: '',
+        fullName: '',
+        password: '',
+        imgUrl: '',
+      }
     })
   }
 
-  const onUploadImg = async (ev) => {
-    setIsLoading(true)
+  onUploadImg = async (ev) => {
+    this.setState({ isLoading: true })
     let userImgUrl = await CloudinaryService.uploadImg(ev)
-    setNewUser({ ...newUser, imgUrl: userImgUrl.secure_url })
-    setIsLoading(false)
+    this.setState({
+      newUser: {
+        ...this.state.newUser,
+        imgUrl: userImgUrl.secure_url
+      }
+    })
+    this.setState({ isLoading: false })
   }
 
-  const connectSockets = (id) => {
-    props.onConnectSocket(id)
+  connectSockets(id) {
+    this.props.onConnectSocket(id)
   }
 
-  const onHandleSubmit = async (ev) => {
+  toggleSignUp = () => {
+    this.setState(prevState => ({
+      isSignUp: !prevState.isSignUp
+    }));
+  }
+
+  onHandleSubmit = async (ev) => {
     ev.preventDefault();
-    setIsDone(true)
-    isSignup ? await props.signUp({ ...newUser }) : await props.login({ ...newUser })
-     await getLoggedinUser()
-    loggedinUser = {...props.user}
-    if (!loggedinUser) {
+    this.setState({ isDone: true })
+    this.state.isSignUp ?
+      await this.props.signUp({ ...this.state.newUser })
+      : await this.props.login({ ...this.state.newUser })
+    await this.getLoggedinUser();
+    if (!this.props.user) {
       Swal.fire({
         title: 'Wrong password or Username.',
         width: 300,
@@ -74,37 +105,38 @@ const SignupPage = (props) => {
         no-repeat
         `
       })
-      props.history.push("/signup")
-      resetInput()
-      setIsDone(false)
+      this.props.history.push("/signup")
+      this.resetInput()
+      this.setState({ isDone: false })
       return
     }
-    props.showNotification('Logged in successfully :)', 'success')
-    connectSockets(loggedinUser._id)
-    console.log(loggedinUser);
-    props.history.push("/")
+    this.props.showNotification('Logged in successfully :)', 'success')
+    this.connectSockets(this.props.user._id)
+    this.props.history.push("/")
+
   }
 
-  useEffect(() => {
-    getLoggedinUser()
-  }, [])
 
-  return (
-    isDone ? <Loading /> :
-      <div className="signup-form">
-        <form onSubmit={(e) => onHandleSubmit(e)}>
-          {isSignup ? <h1>SignUp</h1> : <h1>Login</h1>}
-          {isSignup && <AvatarEdit onUploadImg={onUploadImg} imgUrl={newUser.imgUrl} isLoading={isLoading} />}
-          {isSignup && <MDBInput label="Full Name" name="fullName" value={newUser.fullName || ''} onChange={e => setNewUser({ ...newUser, fullName: e.target.value })} />}
-          <MDBInput label="UserName" type="text" name="userName" value={newUser.userName || ''} onChange={e => setNewUser({ ...newUser, userName: e.target.value })} />
-          <MDBInput label="Password" type="password" name="password" value={newUser.password || ''} onChange={e => setNewUser({ ...newUser, password: e.target.value })} />
-          <button className="signup-login-btn">{isSignup ? 'Sign up' : 'Login'}</button>
-          {!isSignup && <h2 >Don't Have an account?<span className="toggle-btn" onClick={() => setIsSignup(!isSignup)}> Sign up!</span></h2>}
-          {isSignup && <h2 >Head Back to <span className="toggle-btn" onClick={() => setIsSignup(!isSignup)}>Login</span></h2>}
-        </form>
-      </div>
-  );
 
+  render() {
+    const { isSignUp, isLoading, isDone } = this.state
+    const { imgUrl } = this.state.newUser
+    return (
+      isDone ? <Loading /> :
+        <div className="signup-form">
+          <form onSubmit={this.onHandleSubmit}>
+            {isSignUp ? <h1>SignUp</h1> : <h1>Login</h1>}
+            {isSignUp && <AvatarEdit onUploadImg={this.onUploadImg} imgUrl={imgUrl} isLoading={isLoading} />}
+            {isSignUp && <MDBInput label="Full Name" name="fullName" value={this.state.newUser.fullName || ''} onChange={this.onChangeHandler} />}
+            <MDBInput label="UserName" type="text" name="userName" value={this.state.newUser.userName || ''} onChange={this.onChangeHandler} />
+            <MDBInput label="Password" type="password" name="password" value={this.state.newUser.password || ''} onChange={this.onChangeHandler} />
+            <button className="signup-login-btn">{isSignUp ? 'Sign up' : 'Login'}</button>
+            {!isSignUp && <h2 >Don't Have an account?<span className="toggle-btn" onClick={this.toggleSignUp}> Sign up!</span></h2>}
+            {isSignUp && <h2 >Head Back to <span className="toggle-btn" onClick={this.toggleSignUp}>Login</span></h2>}
+          </form>
+        </div>
+    );
+  }
 }
 
 
@@ -123,7 +155,9 @@ const mapDispatchToProps = {
 export default connect(mapStateToProps, mapDispatchToProps)(SignupPage);
 
 
-// import React, { Component } from "react";
+
+//func version:
+// import React, { useState, useEffect } from "react";
 // import { MDBInput } from "mdbreact";
 // import Loading from "../cmps/Loading";
 
@@ -138,82 +172,53 @@ export default connect(mapStateToProps, mapDispatchToProps)(SignupPage);
 // import { AvatarEdit } from '../cmps/User/AvatarEdit'
 
 
-// class SignupPage extends Component {
+// const SignupPage = (props) => {
+//   const [isSignup, setIsSignup] = useState(false)
+//   const [isLoading, setIsLoading] = useState(false)
+//   const [isDone, setIsDone] = useState(false)
+//   const [newUser, setNewUser] = useState({
+//     userName: '',
+//     fullName: '',
+//     password: '',
+//     imgUrl: '',
+//   })
+//   let loggedinUser = null
 
-//   state = {
-//     newUser: {
+//   const getLoggedinUser = async () => {
+//     await props.getUser()
+//     if (props.user) props.history.push('/')
+//     return props.user
+//   }
+
+
+
+//   const resetInput = () => {
+//     setNewUser({
 //       userName: '',
 //       fullName: '',
 //       password: '',
 //       imgUrl: '',
-//     },
-//     isSignUp: false,
-//     isLoading: false,
-//     isDone: false
-//   }
-
-//   componentDidMount() {
-//     console.log('hi from signUp', this.state.newUser);
-
-//     this.getLoggedinUser();
-//   }
-
-//   getLoggedinUser = async () => {
-//     await this.props.getUser()
-//     if (this.props.user) this.props.history.push('/')
-//   }
-
-//   onChangeHandler = (ev) => {
-//     const { value, name } = ev.target;
-//     this.setState({
-//       newUser: {
-//         ...this.state.newUser,
-//         [name]: value
-//       }
-//     })
-//   };
-
-//   resetInput = () => {
-//     this.setState({
-//       newUser: {
-//         userName: '',
-//         fullName: '',
-//         password: '',
-//         imgUrl: '',
-//       }
 //     })
 //   }
 
-//   onUploadImg = async (ev) => {
-//     this.setState({ isLoading: true })
+//   const onUploadImg = async (ev) => {
+//     setIsLoading(true)
 //     let userImgUrl = await CloudinaryService.uploadImg(ev)
-//     this.setState({
-//       newUser: {
-//         ...this.state.newUser,
-//         imgUrl: userImgUrl.secure_url
-//       }
-//     })
-//     this.setState({ isLoading: false })
+//     setNewUser({ ...newUser, imgUrl: userImgUrl.secure_url })
+//     setIsLoading(false)
 //   }
 
-//   connectSockets(id) {
-//     this.props.onConnectSocket(id)
+//   const connectSockets = (id) => {
+//     props.onConnectSocket(id)
 //   }
 
-//   toggleSignUp = () => {
-//     this.setState(prevState => ({
-//       isSignUp: !prevState.isSignUp
-//     }));
-//   }
-
-//   onHandleSubmit = async (ev) => {
+//   const onHandleSubmit = async (ev) => {
 //     ev.preventDefault();
-//     this.setState({ isDone: true })
-//     this.state.isSignUp ?
-//       await this.props.signUp({ ...this.state.newUser })
-//       : await this.props.login({ ...this.state.newUser })
-//     await this.getLoggedinUser();
-//     if (!this.props.user) {
+//     setIsDone(true)
+//     isSignup ? await props.signUp({ ...newUser }) : await props.login({ ...newUser })
+//     await getLoggedinUser()
+//     loggedinUser = { ...props.user }
+//     if (!loggedinUser.userName) {
 //       Swal.fire({
 //         title: 'Wrong password or Username.',
 //         width: 300,
@@ -228,37 +233,37 @@ export default connect(mapStateToProps, mapDispatchToProps)(SignupPage);
 //         no-repeat
 //         `
 //       })
-//       this.props.history.push("/signup")
-//       this.resetInput()
-//       this.setState({ isDone: false })
+//       props.history.push("/signup")
+//       resetInput()
+//       setIsDone(false)
 //       return
 //     }
-//     this.connectSockets(this.props.user._id)
-//     this.props.history.push("/")
-
+//     props.showNotification('Logged in successfully :)', 'success')
+//     connectSockets(loggedinUser._id)
+//     console.log(loggedinUser);
+//     props.history.push("/")
 //   }
 
+//   useEffect(() => {
+//     getLoggedinUser()
+//   }, [])
 
+//   return (
+//     isDone ? <Loading /> :
+//       <div className="signup-form">
+//         <form onSubmit={(e) => onHandleSubmit(e)}>
+//           {isSignup ? <h1>SignUp</h1> : <h1>Login</h1>}
+//           {isSignup && <AvatarEdit onUploadImg={onUploadImg} imgUrl={newUser.imgUrl} isLoading={isLoading} />}
+//           {isSignup && <MDBInput label="Full Name" name="fullName" value={newUser.fullName || ''} onChange={e => setNewUser({ ...newUser, fullName: e.target.value })} />}
+//           <MDBInput label="UserName" type="text" name="userName" value={newUser.userName || ''} onChange={e => setNewUser({ ...newUser, userName: e.target.value })} />
+//           <MDBInput label="Password" type="password" name="password" value={newUser.password || ''} onChange={e => setNewUser({ ...newUser, password: e.target.value })} />
+//           <button className="signup-login-btn">{isSignup ? 'Sign up' : 'Login'}</button>
+//           {!isSignup && <h2 >Don't Have an account?<span className="toggle-btn" onClick={() => setIsSignup(!isSignup)}> Sign up!</span></h2>}
+//           {isSignup && <h2 >Head Back to <span className="toggle-btn" onClick={() => setIsSignup(!isSignup)}>Login</span></h2>}
+//         </form>
+//       </div>
+//   );
 
-//   render() {
-//     const { isSignUp, isLoading, isDone } = this.state
-//     const { imgUrl } = this.state.newUser
-//     return (
-//       isDone ? <Loading /> :
-//         <div className="signup-form">
-//           <form onSubmit={this.onHandleSubmit}>
-//             {isSignUp ? <h1>SignUp</h1> : <h1>Login</h1>}
-//             {isSignUp && <AvatarEdit onUploadImg={this.onUploadImg} imgUrl={imgUrl} isLoading={isLoading} />}
-//             {isSignUp && <MDBInput label="Full Name" name="fullName" value={this.state.newUser.fullName || ''} onChange={this.onChangeHandler} />}
-//             <MDBInput label="UserName" type="text" name="userName" value={this.state.newUser.userName || ''} onChange={this.onChangeHandler} />
-//             <MDBInput label="Password" type="password" name="password" value={this.state.newUser.password || ''} onChange={this.onChangeHandler} />
-//             <button className="signup-login-btn">{isSignUp ? 'Sign up' : 'Login'}</button>
-//             {!isSignUp && <h2 >Don't Have an account?<span className="toggle-btn" onClick={this.toggleSignUp}> Sign up!</span></h2>}
-//             {isSignUp && <h2 >Head Back to <span className="toggle-btn" onClick={this.toggleSignUp}>Login</span></h2>}
-//           </form>
-//         </div>
-//     );
-//   }
 // }
 
 
@@ -275,7 +280,6 @@ export default connect(mapStateToProps, mapDispatchToProps)(SignupPage);
 // };
 
 // export default connect(mapStateToProps, mapDispatchToProps)(SignupPage);
-
 
 
 // //functional handle submit:
