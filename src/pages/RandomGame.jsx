8a3e9d5dc3b387d561b36053951f4dae4a3cf981
Your NameTamir <tamirn7@gmail.com>
2020-09-06 18:27:46 +0300

    fixed game page

--- src/pages/RandomGame.jsx
+++ src/pages/RandomGame.jsx
@@ -6,26 +6,27 @@ import React,{ useState, useEffect } from 'react';
 
 
 const RandomGame = () => {
-    const [gameUrl, setGameUrl] = useState('')
+    const [gameName, setGameName] = useState('')
 
-    const games = [{name:'Tetris', url:'https://chvin.github.io/react-tetris/'}, {name:'Packman', url:"https://pacman-e281c.firebaseapp.com/"}, {name:'Space Invaders', url:"https://arcade.ly/games/space-invaders/"}]
+    const gamesName = ['tetrix-en-s-iga-cloud','little-jump-guy-new-en-s-iga-cloud','bottle-flip-new-en-s-iga-cloud', 'basket-fall2-new-en-s-iga-cloud','easter-hunt-new-en-s-iga-cloud','trump-run-new-en-s-iga-cloud','minigolf-new-en-s-iga-cloud','onion-boy-new-en-s-iga-cloud','tank-wars-new-en-s-iga-cloud','angry-chicken-egg-madness-new-en-s-iga-cloud','switch-sides-new-en-s-iga-cloud','html5/unlockit-en-s-iga-cloud', 'spotle-en-s-iga-cloud','snake-condo-en-s-iga-cloud', 'cheese-lab-en-s-iga-cloud','medieval-dodgeball-en-s-iga-cloud']
+     
 
     const setGame = () =>{
-        let randomInt = Math.floor(Math.random() * games.length) 
-        setGameUrl(games[randomInt].url)
-        console.log(gameUrl);
+        let randomInt = Math.floor(Math.random() * gamesName.length) 
+        setGameName(gamesName[randomInt])
+        console.log(gameName);
     }
 
     useEffect(() => {
         setGame()
         return () => {
-            setGameUrl('')
+            setGameName('')
         };
     }, [])
 
 
     return <div className="random-game">
-        {gameUrl &&<Iframe url={gameUrl} width="100%" height="100%" className="iframe" />}
+        {gameName &&<Iframe url={`https://wanted5games.com/games/html5/${gameName}/index.html?pub=10`} width="100%" height="100%" className="iframe" />}
     </div>;
 }
 
