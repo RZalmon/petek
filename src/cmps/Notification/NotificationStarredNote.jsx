ede082ca42a65559651a2840c9bc8060670bd446
Your NameTamir <tamirn7@gmail.com>
2020-10-21 11:38:52 +0300

    push to pull

---
+++ src/cmps/Notification/NotificationStarredNote.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default ({ notification }) => {
+    return (
+        <div className="notification-starred-note">
+            <p>
+                
+            </p>
+        </div>
+    )
+}
