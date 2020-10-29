706ff6802d9dc75fa5bf23a215e0a0d69a3c399f
RZalmon <ranzlmn@gmail.com>
2020-10-29 14:33:24 +0200

    spread view and read more

---
+++ src/cmps/LongText.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react'
+
+export default ({ text, limit }) => {
+    const [isLongText, setIsLongText] = useState('')
+    const [textToShow, setTextToShow] = useState('')
+
+    const toggleIsLongText = () => {
+        setIsLongText(!isLongText)
+        setText()
+    }
+
+    const setText = () => {
+        isLongText ? setTextToShow(text.substring(0, limit) + '...') : setTextToShow(text)
+    }
+
+    useEffect(() => {
+        setIsLongText(text.length > limit)
+    }, [])
+
+    useEffect(() => {
+        setText()
+    }, [isLongText])
+
+    return (
+        <div className="long-text">
+            {textToShow.length && <p className="long-text" >
+                {textToShow}
+                {text.length > limit && <span class="long-text-btn" onClick={toggleIsLongText}>{textToShow.length > limit + 4 ? 'Read Less' : 'Read More'}</span>}
+            </p>}
+        </div>
+    )
+}
