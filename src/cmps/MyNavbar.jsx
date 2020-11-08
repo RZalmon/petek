b8813a387c69353885f0f88c4602ad58ce118092
RZalmon <ranzlmn@gmail.com>
2020-11-08 13:11:08 +0200

    navbar fixes

--- src/cmps/MyNavbar.jsx
+++ src/cmps/MyNavbar.jsx
@@ -1,24 +1,18 @@
-import React, { createRef, useEffect } from 'react'
+import React, { createRef, useEffect, useState } from 'react'
 import { Link, NavLink, withRouter } from 'react-router-dom';
 
 import logo from '../../src/assets/png/petek-logo.png';
 
 
 export default ({ user, handleLogout, history }) => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
     const checkboxRef = createRef()
 
-    // useEffect(() => {//Closing The menu after click
-    //    console.log(history.location);
-    //      checkboxRef.current.checked = false;
-    // }, [history.location.pathname])
+    const handleCheboxClicked = (bool) => {
+        setIsMenuOpen(bool)
+        if (checkboxRef.current) checkboxRef.current.checked = bool
+    }
 
-useEffect(() => {
-      return history.listen((location) => { 
-          console.log(checkboxRef);
-        //   if(checkboxRef && checkboxRef.current) checkboxRef.current.checked = false;
-         console.log(`You changed the page to: ${location.pathname}`) 
-      }) 
-   },[history])
 
     return (
         <nav className="main-nav">
@@ -28,35 +22,35 @@ useEffect(() => {
                     <img src={logo} alt="logo" className="logo" />
                 </Link>
 
-                <input type="checkbox" id="mobile-nav" ref={checkboxRef} hidden />
-               { user &&<label htmlFor="mobile-nav" className="mobile-btn">
+                <input type="checkbox" id="mobile-nav" className={isMenuOpen ? 'menu-open' : ''} ref={checkboxRef} hidden onClick={() => handleCheboxClicked(!isMenuOpen)} />
+                {user && <label htmlFor="mobile-nav" className="mobile-btn">
                     <span>|</span>
                 </label>}
 
                 {user && <ul className="link-list">
                     <li>
-                        <NavLink to="/" className="link" exact>
+                        <NavLink to="/" className="link" exact onClick={() => { handleCheboxClicked(false) }}>
                             Home
                          </NavLink>
                     </li>
                     <li>
-                        <NavLink to="/contact" className="link" exact>
+                        <NavLink to="/contact" className="link" exact onClick={() => { handleCheboxClicked(false) }}>
                             Contacts
                          </NavLink>
                     </li>
                     <li>
                         {user.notifications.length && <span className="notification-count">{user.notifications.length}</span>}
-                        <NavLink to={`/inbox/${user._id}`} className="link" exact>
+                        <NavLink to={`/inbox/${user._id}`} className="link" exact onClick={() => { handleCheboxClicked(false) }}>
                             Inbox
                          </NavLink>
                     </li>
                     <li>
-                        <NavLink to={`/random-game`} className="link" exact>
+                        <NavLink to={`/random-game`} className="link" exact onClick={() => { handleCheboxClicked(false) }}>
                             Random Game
                          </NavLink>
                     </li>
                     <li>
-                        <NavLink to="/starred" className="link" exact>
+                        <NavLink to="/starred" className="link" exact onClick={() => { handleCheboxClicked(false) }}>
                             Starred
                          </NavLink>
                     </li>
